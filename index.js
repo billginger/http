fbdb8ef64df96d0bc135c6adc84c9e777754c55b
Bill <jfg@163.com>
2017-11-19 23:24:56 +0800

    redirect nodejs https

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const hostA = [
 	'ourhome.pub', 'www.ourhome.pub', 'starwars.pub', 'www.starwars.pub',
 	'wars.pub', 'www.wars.pub', 'kof.pub', 'www.kof.pub', 'kom.pub', 'www.kom.pub'
 ];
-const hostB = 'jz.kom.pub';
+const hostB = 'nodejs.top';
 
 http.createServer((req, res) => {
 	if (hostA.indexOf(req.headers.host) >= 0) {
 		proxy.web(req, res, { target: 'http://localhost:3001' });
-	} else if (hostB.indexOf(req.headers.host) >= 0) {
-		proxy.web(req, res, { target: 'http://localhost:3002' });
+	} else if (req.headers.host.indexOf(hostB) >= 0) {
+		res.writeHead(301, { 'Location': 'https://nodejs.top' });
+		res.end();
 	} else {
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write('<h1>Node.js</h1>');
